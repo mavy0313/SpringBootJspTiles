2b753cd81d233d0f4d08371daeb88ef316d9d1a1
Maxim Vypov <mavy0313@netcracker.com>
2018-07-16 18:49:59 +0400

    initial commit

---
+++ src/main/java/org/o7planning/sbtiles/SpringBootJspTilesApplication.java
@@ -0,0 +1,12 @@
+package org.o7planning.sbtiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJspTilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJspTilesApplication.class, args);
+	}
+}
